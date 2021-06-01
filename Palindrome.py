ef874bc5d9eb9bd91557c7aef3513afdeb19b124
Ryan Ong <ongr@oregonstate.edu>
2021-06-01 16:43:13 -0700

    finished with palindrome

--- Palindrome.py
+++ Palindrome.py
@@ -22,12 +22,39 @@ def checkPalindrome_1(string, k):
         return False
 
 
-# Improved
+# Dynamic programming with top down approach with memoization.
 def checkPalindrome_2(string, k):
-    pass
+    string_memo = {}
+    return helper_2(string, k, string_memo)
+
+
+# Helper function for improved palindrome approach.
+def helper_2(string, k, string_memo):
+    # Not a palindrome if empty.
+    if string == "":
+        return False
+
+    # Is this an overlapping sub-problem that has been solved?
+    if string in string_memo:
+        return False
+
+    # Is the current substring a palindrome?
+    if string == string[::-1]:
+        return True
+
+    # Checks if substring is a palindrome for at least one child substring up to k characters removed from original
+    if k > 0:
+        for position in range(0, len(string)):
+            sub_string = string[:position] + string[position + 1:]
+            if checkPalindrome_1(sub_string, k - 1):
+                return True
+
+    # No matches found
+    string_memo[string] = False
+    return False
 
 
 if __name__ == "__main__":
     input_string = "abcba"
     input_number = 3
-    print(checkPalindrome_1(input_string, input_number))
+    print(checkPalindrome_2(input_string, input_number))
--- tests_palindrome.py
+++ tests_palindrome.py
@@ -1,57 +1,57 @@
 import unittest
-from Palindrome import checkPalindrome_1
+from Palindrome import checkPalindrome_2
 
 
 class MyTestCase(unittest.TestCase):
     def test_example1(self):
         input_string = "abcba"
         input_number = 3
-        self.assertEqual(True, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(True, checkPalindrome_2(input_string, input_number))
 
     def test_example2(self):
         input_string = "abcdeba"
         input_number = 2
-        self.assertEqual(True, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(True, checkPalindrome_2(input_string, input_number))
 
     def test_personal1(self):
         input_string = ""
         input_number = 10
-        self.assertEqual(False, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(False, checkPalindrome_2(input_string, input_number))
 
     def test_personal2(self):
         input_string = "a"
         input_number = 10
-        self.assertEqual(True, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(True, checkPalindrome_2(input_string, input_number))
 
     def test_personal3(self):
         input_string = "ab"
         input_number = 0
-        self.assertEqual(False, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(False, checkPalindrome_2(input_string, input_number))
 
     def test_personal4(self):
         input_string = "ab"
         input_number = 1
-        self.assertEqual(True, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(True, checkPalindrome_2(input_string, input_number))
 
     def test_personal5(self):
         input_string = "cbba"
         input_number = 1
-        self.assertEqual(False, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(False, checkPalindrome_2(input_string, input_number))
 
     def test_personal6(self):
         input_string = "eabcdeecba"
         input_number = 3
-        self.assertEqual(True, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(True, checkPalindrome_2(input_string, input_number))
 
     def test_personal7(self):
         input_string = "eabcdeecba"
         input_number = 2
-        self.assertEqual(True, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(True, checkPalindrome_2(input_string, input_number))
 
     def test_personal8(self):
         input_string = "abcd"
         input_number = 2
-        self.assertEqual(False, checkPalindrome_1(input_string, input_number))
+        self.assertEqual(False, checkPalindrome_2(input_string, input_number))
 
 
 if __name__ == '__main__':
